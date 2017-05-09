beb3643aa87eaff4af8e743baab89c64b19e35f5
hav <16089647+haversin@users.noreply.github.com>
2017-05-09 03:00:35 +0000

    velocities, ale nie zbiega do adamsa, poprawa

---
+++ matlaby/simulate.m
@@ -0,0 +1,14 @@
+function [ time, dis, vel ] = simulate( Fi_qt, Fiq_q, Fit_t, q0, end_time, n_steps )
+    step = end_time/n_steps;
+    dis = zeros(length(q0),n_steps);
+    vel = zeros(length(q0),n_steps);
+    time = [0:step:end_time];
+    
+    for i=1:(n_steps+1)
+        Fi_q = @(q) Fi_qt(q,time(i));
+        dis(:,i) = newtonify(Fi_q, Fiq_q, q0); % do magic
+        %Fit_t(time(i))'
+        vel(:,i) = -Fiq_q(dis(:,i))\Fit_t(time(i))';
+    end
+    %dis = answer;
+end
