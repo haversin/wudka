4731336c1d941e92dab8858faad87d23817a1cbe
natalke <natalke@b34af178-8191-4814-9229-549340ca6d4d>
2017-05-09 19:47:38 +0000

    poprawiony calc_marker.m

--- matlaby/calc_marker.m
+++ matlaby/calc_marker.m
@@ -4,11 +4,11 @@ omega = [0 -1; 1 0];
 phi = dis(3);
 dphi = vel(3);
 ddphi = acc(3);
-R = rotz(phi);
 
-dis_point = dis(1:2) + R*s(1:2);
-vel_point = vel(1:2) + omega*R*dphi;
-acc_point = acc(1:2) + omega*omega*R*dphi*dphi*s(1:2) + omega*R*ddphi*s(1:2);
+
+dis_point = dis(1:2) + R(phi)*s(1:2);
+vel_point = vel(1:2) + omega*R(phi)*dphi;
+acc_point = acc(1:2) + omega*omega*R(phi)*dphi*dphi*s(1:2) + omega*R(phi)*ddphi*s(1:2);
 
 end
 
