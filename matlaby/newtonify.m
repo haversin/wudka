4727bc78b1aa7d2b6a1024736ba4fd986642747f
hav <hav@b34af178-8191-4814-9229-549340ca6d4d>
2017-05-09 03:00:35 +0000

    velocities, ale nie zbiega do adamsa, poprawa

---
+++ matlaby/newtonify.m
@@ -0,0 +1,12 @@
+function [ q ] = newtonify( Fi, Fiq, q0)
+    %dupa = fsolve(Fi,q0)
+    dupa = ones(length(q0),1);
+    q = q0;
+    it = 0;
+    while norm(dupa) > 1e-16 && it < 100
+        dupa = Fi(q);
+        jacobi = Fiq(q);
+        q = q - jacobi\dupa;
+        it = it + 1;
+    end
+end
